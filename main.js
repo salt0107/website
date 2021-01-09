6bd45d483e07dac76a4ef924d8aa41b3867abd14
salt0107 <60471825+salt0107@users.noreply.github.com>
2021-01-09 16:37:13 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function showMessage(){
+	alert('장바구니에 담겼습니다.');
+}	
+function changeSize(){
+	let imgElement = document.querySelector("img");
+	imgElement.style.width = "300px";
+}
+function changeSizeOriginal(){
+	let imgElement = document.querySelector("img");
+	imgElement.style.width = "280px";
+}			
